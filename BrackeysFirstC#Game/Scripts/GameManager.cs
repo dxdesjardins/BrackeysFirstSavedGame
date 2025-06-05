a676509986ef5c9e15de8f34d96fc45dc5f4a29c
Dylan Desjardins <72995117+dxdesjardins@users.noreply.github.com>
2025-06-04 22:56:50 -0400

    Add files via upload

---
+++ BrackeysFirstC#Game/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+
+namespace FirstGame.Scripts;
+
+public sealed partial class GameManager : Node
+{
+    private int _score;
+    private Label _scoreLabel = default!;
+    
+    public override void _Ready()
+    {
+        _score = 0;
+        _scoreLabel = this.GetNodeOrThrow<Label>("ScoreLabel");
+    }
+
+    public void AddPoint()
+    {
+        _score++;
+        _scoreLabel.Text = $"You collected {_score} coins.";
+    }
+}
---
+++ BrackeysFirstC#Game/Scripts/KillZone.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+
+namespace FirstGame.Scripts;
+
+public sealed partial class KillZone : Area2D
+{
+    private const double NormalTimeScale = 1D;
+    private const double SlowTimeScale = NormalTimeScale / 2D;
+    private Timer _timer = default!;
+    
+    public override void _Ready()
+    {
+        _timer = this.GetNodeOrThrow<Timer>("Timer");
+    }
+    
+    private void HandleTimerTimeout()
+    {
+        Engine.TimeScale = NormalTimeScale;
+        GetTree().ReloadCurrentScene();
+    }
+    
+    private void HandleBodyEntered(Node2D body)
+    {
+        GD.Print("You died!");
+        Engine.TimeScale = SlowTimeScale;
+        body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
+        _timer.Start();
+    }
+}
